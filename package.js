28de8123d045e0501833a00ca4191a047d407073
Lawrence Wakefield <lawrence@iamlawrence.me>
2016-02-29 12:56:28 -0500

    fixd version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lwakefield:vue',
-  version: '1.0',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Simple binding to Vue.js for Meteor',
   // URL to the Git repository containing the source code for this package.
